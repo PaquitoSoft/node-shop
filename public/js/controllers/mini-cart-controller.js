6ebcc4056285259236742337e2ae7f2e9ee85708
PaquitoSoft <paquitosoftware@gmail.com>
2014-12-11 23:51:07 +0100

    Mini-cart count finished

--- public/js/controllers/mini-cart-controller.js
+++ public/js/controllers/mini-cart-controller.js
@@ -1,6 +1,6 @@
 (function() {
 	// MiniCartController
-	define(['jquery', 'plugins/events-manager', 'plugins/local-storage'], function($, events, storage) {
+	define(['jquery', 'plugins/events-manager'/*, 'plugins/local-storage'*/], function($, events, storage) {
 		var $itemsCount;
 
 		// TODO Retrieve mini-cart if local info has expired
@@ -13,16 +13,18 @@
 		}
 
 		function _onProductAddedToCart() {
-			var miniCart = storage.retrieve('mini-cart') || { count: 0 };
-			miniCart.count += 1;
-			storage.store('mini-cart', miniCart, { ttl: 1500 });
-			$itemsCount.text(miniCart.count);
+			// var miniCart = storage.retrieve('mini-cart') || { count: 0 };
+			// miniCart.count += 1;
+			// storage.store('mini-cart', miniCart, { ttl: 1500 });
+			// $itemsCount.text(miniCart.count);
+
+			$itemsCount.text(parseInt($itemsCount.text(), 10) + 1);
 		}
 
 		function configure($mainEl) {
 			$itemsCount = $mainEl.find('._items-count');
 
-			_initUI();
+			// _initUI();
 			events.on('productAddedToCart', _onProductAddedToCart);
 
 			console.log('MiniCartController initialized!');
