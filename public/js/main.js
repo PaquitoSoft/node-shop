700051f5f27bf13bdf8ea7aa1228bc345116f9e7
PaquitoSoft <paquitosoftware@gmail.com>
2014-12-19 00:32:19 +0100

    Created the chance for a notification when al controllers are loaded

--- public/js/main.js
+++ public/js/main.js
@@ -10,19 +10,39 @@
 	});
 	
 	// Main initialization
-	require(['jquery', 'plugins/dom'], function($, dom) {
+	require(['jquery'], function($) {
 		var controllers = [],
-			mainElements = [];
+			mainElements = [],
+			onControllerInitialized,
+			pendingControllers;
 		
+		onControllerInitialized = function() {
+			if (pendingControllers > 1) {
+				pendingControllers--;
+			} else {
+				console.log('ALL Controllers han been fully initialized!');
+			}
+		}
+
 		$('*[data-controller]').each(function(index, elem) {
 			controllers.push('controllers/' + $(elem).attr('data-controller'));
 			mainElements.push($(elem));
 		});
 
+		pendingControllers = controllers.length;
+
 		require(controllers, function() {
+			
 			$.each(arguments, function(index, controller) {
 				try {
-					controller.init(mainElements[index]);
+					if (controller.init.length > 1) {
+						controller.init(mainElements[index], onControllerInitialized);
+					} else {
+						controller.init(mainElements[index]);
+						onControllerInitialized();
+					}
+					// TODO Maybe we should have a timeout warning if some controller gets stuck
+					//		or someone included callback but forgot to call it
 				} catch (e) {
 					console.log('Error initializing controller %s: %s', controller, e);
 					console.log(e.stack);
