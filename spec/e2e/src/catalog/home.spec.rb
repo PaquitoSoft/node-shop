605a6d48311bda16e8345680db922a13dd0973e7
PaquitoSoft <paquitosoftware@gmail.com>
2015-06-20 20:16:12 +0200

    Implemented three basic e2e tests with ruby Watir

--- spec/e2e/src/catalog/home.spec.rb
+++ spec/e2e/src/catalog/home.spec.rb
@@ -2,39 +2,61 @@ require 'rspec'
 require 'watir-webdriver'
 
 describe 'Home Page' do	
-	let(:browser) { @browser ||= Watir::Browser.new }
-	let(:root_categories_selector) { @root_categories_selector ||= '#main-menu li' }
-	let(:subcategories_groups_selector_hidden) { @subcategories_groups_selector_hidden ||= '#main-menu li .accordion-content.invisible' }
-	let(:main_products_selector) { @main_products_selector ||= '.product-grid ._product-summary' }
 	
-	def check_initial_state
-		els = browser.elements :css => root_categories_selector
-		expect(els.length).to eq 3
-		els = browser.elements :css => subcategories_groups_selector_hidden
-		expect(els.length).to eq 3
-		els = browser.elements :css => main_products_selector
-		expect(els.length).to eq 9
-	end
+	$browser = nil	
 
-	def get_elements(selector, parent_element = browser)
+	def get_elements(selector, parent_element = $browser)
 		parent_element.elements :css => selector
 	end
 
-	def get_element(selector, parent_element = browser)
-		get_elements(selector, parent_element)[0]
+	def get_element(selector, parent_element = $browser, wait = false)
+		wait_for_element if wait
+		parent_element.element :css => selector
 	end
 
-	def get_random_element(elementsOrSelector, parent_element = browser)
+	def get_random_element(elementsOrSelector, parent_element = $browser)
 		elements = elementsOrSelector
 		if elements.instance_of? String then
 			elements = get_elements(elementsOrSelector, parent_element)
 		end
-		elements[rand(0..elements.length)]
+		elements[rand(elements.length)]
 	end
 
-	before { browser.goto 'http://localhost:3000' }
+	def wait_for_element(selector)
+		get_element(selector).wait_until_present(5)
+	end
 
-	after { browser.close }
+	def click_element(elementOrSelector, parent_element = $browser)
+		element = elementOrSelector
+		if element.instance_of? String then
+			element = get_element(elementOrSelector, parent_element)
+		end
+		element.wait_until_present(5)
+		element.click
+	end
+
+	def check_initial_state
+		expect(get_elements('#main-menu li').length).to eq 3
+		expect(get_elements('#main-menu li .accordion-content.invisible').length).to eq 3
+		expect(get_elements('.product-grid ._product-summary').length).to eq 9
+	end
+
+	before(:all) do
+		$browser = Watir::Browser.new :chrome
+	end
+
+	before(:each) do
+		$browser.goto 'http://localhost:3000'
+		$browser.wait
+	end
+
+	# after(:each) do
+	# 	$browser.reset!
+	# end
+
+	after(:all) do
+		$browser.close
+	end
 
 	it 'Should load home page' do
 
@@ -42,34 +64,46 @@ describe 'Home Page' do
 		check_initial_state()
 
 		# Click a random root category
-		get_random_element('#main-menu li .accordion-button').click
+		click_element('#main-menu li .accordion-button')
 
 		# Check navigation tree is unfolded
-		expect(get_elements(subcategories_groups_selector_hidden).length).to eq 2
+		expect(get_elements('#main-menu li .accordion-content.invisible').length).to eq 2
 
 	end
 
-	xit 'Should load home page and navigate to one category' do
+	it 'Should load home page and navigate to one category' do
 
 		# Check initial state
 		check_initial_state()
 
 		# Get and click a random root category
+		root_category = get_random_element('#main-menu li')
+		puts "Selected root category: #{root_category.text}"
+		click_element('.accordion-button', root_category)
 
 		# Get and click a random subcategory
+		subcategory = get_random_element('._subcategory', root_category)
+		click_element(subcategory)
 
-		# Check we've navigated to category page (page heading title matches and has products count element)		
+		# Check we've navigated to category page (page heading title matches and has products count element)
+		get_element('.products-count').wait_until_present
+		expect(get_element('#page-content > h1').text).to eq subcategory.text
 		
 	end
 
-	xit 'Should load home page and navigate to one main product' do
-		
+	it 'Should load home page and navigate to one main product' do
+
 		# Check initial state
 		check_initial_state()
 
 		# Get and click a random main product
+		product = get_random_element('.product-grid ._product-summary')
+		product_name = get_element('h3', product).text
+		click_element(product)
 
 		# Check we've navigated to product page (product name matches)
+		wait_for_element('#page-content.product-detail')
+		expect(get_element('#product-form > h1').text).to eq product_name
 
 	end
 
